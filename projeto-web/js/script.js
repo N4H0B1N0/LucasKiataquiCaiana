f9dbc1ada9ef179d1bb04bb6afb906a3d27bcd60
N4H0B1N0 <127622205+N4H0B1N0@users.noreply.github.com>
2026-09-18 15:56:10 -0300

    Delete projeto-web/js directory

--- projeto-web/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Inicialização segura de Tooltips caso existam no documento
-    if (typeof bootstrap !== 'undefined') {
-        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-        tooltipTriggerList.forEach(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
-    }
-
-    console.log("Site recarregado com sucesso.");
-});
